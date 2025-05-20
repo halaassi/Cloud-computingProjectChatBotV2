1d6c354484555d4159be7084ac9ab2200a791bd5
Hala Assi <hala.assi.2003h@gmail.com>
2025-05-20 22:56:58 +0300

    Added missing index.js for AdminQueries Lambda

---
+++ amplify/backend/function/AdminQueriesd85216a5/src/index.js
@@ -0,0 +1,7 @@
+exports.handler = async (event) => {
+  console.log("AdminQueries Lambda triggered:", JSON.stringify(event));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from AdminQueries Lambda" }),
+  };
+};
